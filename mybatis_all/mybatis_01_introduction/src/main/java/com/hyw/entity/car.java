423045f199ff42432c8826efcf6e7df12a35dde7
jekny <2656803448@qq.com>
2026-04-04 11:21:49 +0800

    通过entity实体类来赋值，然后insert数据库（成功）

---
+++ mybatis_all/mybatis_01_introduction/src/main/java/com/hyw/entity/car.java
@@ -0,0 +1,90 @@
+package com.hyw.entity;
+
+/**
+ * ClassName: car
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/4/4 11:05
+ * @Version 1.0
+ */
+public class car {
+    private int id;
+    private String num;
+    private String brand;
+    private double price;
+    private String time;
+    private String type;
+
+    public car() {
+    }
+
+    public car(int id, String num, String brand, double price, String time, String type) {
+        this.id = id;
+        this.num = num;
+        this.brand = brand;
+        this.price = price;
+        this.time = time;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "car{" +
+                "id=" + id +
+                ", num='" + num + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", time='" + time + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- mybatis_all/mybatis_01_introduction/src/test/java/com/hyw/mybatistest/test02.java
+++ mybatis_all/mybatis_01_introduction/src/test/java/com/hyw/mybatistest/test02.java
@@ -1,5 +1,6 @@
 package com.hyw.mybatistest;
 
+import com.hyw.entity.car;
 import com.hyw.sqlsessionutil.sessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,26 @@ public class test02 {
         map.put("type","rew");
         int count = sqlSession.insert("carinsert2", map);
         if (count==1){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void test03(){
+        car car1 = new car();
+        car1.setId(12);
+        car1.setNum("桂222");
+        car1.setBrand("宝马");
+        car1.setPrice(29);
+        car1.setTime("2025-05-23");
+        car1.setType("燃油");
+        SqlSession sqlSession = sessionUtil.getSqlSession();
+        int count = sqlSession.insert("carinsert2", car1);
+        if (count == 1) {
             System.out.println("yes");
         }else{
             System.out.println("no");
