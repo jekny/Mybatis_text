66bfc361660d82f53fd27026973e151ef30b8363
jekny <2656803448@qq.com>
2026-04-05 11:30:47 +0800

    查询多个（成功）

--- mybatis_all/mybatis_01_introduction/src/test/java/com/hyw/mybatistest/testSelect.java
+++ mybatis_all/mybatis_01_introduction/src/test/java/com/hyw/mybatistest/testSelect.java
@@ -5,6 +5,8 @@ import com.hyw.sqlsessionutil.sessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 /**
  * ClassName: testSelect
  * Description:
@@ -22,5 +24,17 @@ public class testSelect {
         car car1 = (car)sqlSession.selectOne("selectcar1",carcar);
         System.out.println(car1.toString());
         sqlSession.close();
+    }
+
+    @Test
+    public void test02(){
+        SqlSession sqlSession = sessionUtil.getSqlSession();
+        try{
+            List<car> objects = sqlSession.selectList("selectcar2", "京A12345");
+            System.out.println(objects);
+            sqlSession.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
