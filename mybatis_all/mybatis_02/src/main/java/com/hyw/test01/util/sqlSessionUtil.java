e1a636d4de2d6d3bb182c733e607c19dbcb3e7b0
jekny <2656803448@qq.com>
2026-04-05 18:14:02 +0800

    练习，复制了一份结构

---
+++ mybatis_all/mybatis_02/src/main/java/com/hyw/test01/util/sqlSessionUtil.java
@@ -0,0 +1,34 @@
+package com.hyw.test01.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * ClassName: sqlSessionUtil
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/4/5 17:53
+ * @Version 1.0
+ */
+public class sqlSessionUtil {
+        private static SqlSessionFactory factory = null;
+        static {
+                try {
+                        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
+                        factory =  new SqlSessionFactoryBuilder().build(in);
+                } catch (IOException e) {
+                        throw new RuntimeException(e);
+                }
+        }
+
+        public SqlSession getSqlSession(){
+                SqlSession sqlSession = factory.openSession();
+                return sqlSession;
+        }
+}
