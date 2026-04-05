b2aa5fffe3efb6483fd5f32a8ce82ad46188cdeb
jekny <2656803448@qq.com>
2026-04-05 10:40:20 +0800

    查询一条记录成功（有一个要点）
        <select id="selectcar1" resultType="com.hyw.entity.car">
    sql语句配置文件中的resultType是必须写的，我就是因为没写然后错了很久，要告诉mybatis输出结果要封装成什么类型的对象

---
+++ mybatis_all/mybatis_01_introduction/src/test/java/com/hyw/mybatistest/testSelect.java
@@ -0,0 +1,26 @@
+package com.hyw.mybatistest;
+
+import com.hyw.entity.car;
+import com.hyw.sqlsessionutil.sessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+
+/**
+ * ClassName: testSelect
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/4/5 10:22
+ * @Version 1.0
+ */
+public class testSelect {
+    @Test
+    public void test01(){
+        SqlSession sqlSession = sessionUtil.getSqlSession();
+        car carcar = new car();
+        carcar.setId(1);
+        car car1 = (car)sqlSession.selectOne("selectcar1",carcar);
+        System.out.println(car1.toString());
+        sqlSession.close();
+    }
+}
