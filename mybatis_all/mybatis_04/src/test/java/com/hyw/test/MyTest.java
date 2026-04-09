4abbbc7bc43ffce164b8a9750b3a90aeb0566843
jekny <2656803448@qq.com>
2026-04-09 17:33:12 +0800

    开始写测试程序

---
+++ mybatis_all/mybatis_04/src/test/java/com/hyw/test/MyTest.java
@@ -0,0 +1,18 @@
+package com.hyw.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * ClassName: MyTest
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/4/9 17:32
+ * @Version 1.0
+ */
+public class MyTest {
+    @Test
+    public void test01(){
+           
+    }
+}
